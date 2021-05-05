63e356c6699ea7c75ea0ee78918b36945420bf0d
Daniil <ksusonic@yandex.ru>
2021-05-05 12:49:35 +0300

    cookie with username

--- server.py
+++ server.py
@@ -4,6 +4,18 @@ from fastapi.responses import Response
 
 app = FastAPI()
 
+users = {
+    "dan@user.com": {
+        "name": "Даниил",
+        "password": "some_password_1",
+        "balance": 100_000
+    },
+    "petr@user.com": {
+        "name": "Пётр",
+        "password": "some_password_2",
+        "balance": 555_555
+    }
+}
 
 @app.get('/')
 def index_page():
@@ -13,4 +25,12 @@ def index_page():
 
 @app.post('/login')
 def process_login_page(username: str = Form(...), password: str = Form(...)):
-    return Response(f"Ваш логин: {username}, пароль: {password}", media_type="text/html")
+    user = users.get(username)
+    if not user or user["password"] != password:
+        return Response("Я вас не знаю!", media_type="text/html")
+
+    response = Response(
+        f"Привет, {user['name']}<br />Баланс: {user['balance']}", 
+    media_type="text/html")
+    response.set_cookie(key="username", value=username)
+    return response
